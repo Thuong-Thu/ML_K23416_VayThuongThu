db3a7d330d43518091385be5f141f4051fd8833b
Thuong-Thu <thuvt23406@st.uel.edu.vn>
2025-10-08 12:11:56 +0700

    Delete retail_project directory

--- retail_project/test/test_login_mainwindow.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication
-
-from retail_project.uis.LoginMainWindowEx import LoginMainWindowEx
-
-app=QApplication([])
-login_ui=LoginMainWindowEx()
-login_ui.setupUi(QMainWindow())
-login_ui.showWindow()
-app.exec()
